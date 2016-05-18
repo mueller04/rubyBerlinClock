f42e493eb077f2dd8c33e569f94de1f1d47e5ba4
Mike Mueller <mikemueller04@hotmail.com>
2016-05-17 22:30:49 -0400

    passed test_YOOO_returned_for_1_minutes

--- clock.rb
+++ clock.rb
@@ -1,7 +1,21 @@
 class Clock
 
+  def set_time(hour, minute, second)
+    @hour = hour
+    @minute = minute
+    @second = second
+  end
+
   def calculate_time
-      "OOOO"
+    minutes_row = ""
+      @minute.times do
+        minutes_row += "Y"
+      end
+
+      until minutes_row.length == 4 do
+        minutes_row += "O"
+      end
+    return minutes_row
   end
 
 
--- clock_test.rb
+++ clock_test.rb
@@ -5,9 +5,18 @@ class ClockTest < Test::Unit::TestCase
 
   def test_OOOO_returned_for_0_minutes
       clock = Clock.new
-      time = Time.new(2016, 5, 15, 0, 0, 0)
+      clock.set_time(0,0,0)
       result = clock.calculate_time
       assert_equal("OOOO", result)
   end
+
+  def test_YOOO_returned_for_1_minutes
+      clock = Clock.new
+      clock.set_time(0,1,0)
+      result = clock.calculate_time
+      assert_equal("YOOO", result)
+  end
+
+
 
 end
