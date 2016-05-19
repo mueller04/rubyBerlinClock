8058e00656fd86f3f7308c1407e6083746970eae
Mike Mueller <mikemueller04@hotmail.com>
2016-05-19 14:17:52 -0400

    refactor

--- clock.rb
+++ clock.rb
@@ -14,10 +14,15 @@ class Clock
       five_minute_row = @minute / 5
       single_hour_row = @hour
 
-      single_hour_lights = ""
       five_minute_lights = ""
 
-      single_minute_lights = ("Y" * single_minute_row)
+      def switch_lights_on(color, number_lights)
+        color * number_lights
+      end
+
+      single_minute_lights = switch_lights_on("Y", single_minute_row)
+      single_hour_lights = switch_lights_on("R", single_hour_row)
+
 
       def pad_lights_right(desired_length, current_length)
           length_to_pad = desired_length - current_length
@@ -36,7 +41,7 @@ class Clock
 
     five_minute_lights += pad_lights_right(11, five_minute_row)
 
-    single_hour_lights = ("R" * single_hour_row)
+    # single_hour_lights = ("R" * single_hour_row)
 
     single_hour_lights += pad_lights_right(4, single_hour_row)
 
