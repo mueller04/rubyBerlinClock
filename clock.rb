77f21b82697fabd4937134858f19525a1d0dff1d
Mike Mueller <mikemueller04@hotmail.com>
2016-05-19 17:01:29 -0400

    refactor

--- clock.rb
+++ clock.rb
@@ -19,35 +19,49 @@ class Clock
       single_minute_row = @minute % 5
       five_minute_row = @minute / 5
       single_hour_row = @hour
+      five_hour_row = @hour / 5
+      # p
+      # p "foo #{five_hour_row}"
 
-      # def switch_lights_on(color, number_lights)
-      #   color * number_lights
-      # end
-
-      def switch_lights_on(light_pattern, number_lights)
-        light_pattern[0,number_lights]
-      end
-
-      single_minute_lights = switch_lights_on(YELLOW_LIGHTS, single_minute_row)
-      single_hour_lights = switch_lights_on(RED_LIGHTS, single_hour_row)
-
-      def pad_lights_right(desired_length, current_length)
-          length_to_pad = desired_length - current_length
-          "O" * length_to_pad
-      end
-
-      single_minute_lights += pad_lights_right(FOUR_LIGHT_ROW, single_minute_row)
-      single_hour_lights += pad_lights_right(FOUR_LIGHT_ROW, single_hour_row)
+      single_minute_lights = get_lights_for_row(YELLOW_LIGHTS, single_minute_row, FOUR_LIGHT_ROW)
+      single_hour_lights = get_lights_for_row(RED_LIGHTS, single_hour_row, FOUR_LIGHT_ROW)
+      five_minute_lights = get_lights_for_row(MIXED_LIGHTS, five_minute_row, ELEVEN_LIGHT_ROW)
 
 
-      five_minute_lights = switch_lights_on(MIXED_LIGHTS,five_minute_row)
-      five_minute_lights += pad_lights_right(ELEVEN_LIGHT_ROW, five_minute_row)
+      # p "foo2 #{five_hour_row}"
+      # p "foo2 #{five_hour_row}"
 
+      five_hour_lights = ''
+      # five_hour_lights = switch_lights_on(RED_LIGHTS, five_hour_row)
+      # STDOUT.flush
+      # p "foo2 #{five_hour_row}"
+      # p "foo2 #{five_hour_row}"
+      # five_hour_lights += pad_lights_right(FOUR_LIGHT_ROW, five_hour_row)
 
-
-      clock_lamps = ClockLamps.new(single_minute_lights, five_minute_lights, single_hour_lights)
+      clock_lamps = ClockLamps.new(single_minute_lights,
+      five_minute_lights,
+      single_hour_lights,
+      five_hour_lights)
       return clock_lamps
 
+  end
+
+private
+
+  def get_lights_for_row(light_pattern, number_lights, desired_length)
+      return switch_lights_on(light_pattern, number_lights)+ pad_lights_right(desired_length, number_lights)
+  end
+
+  def switch_lights_on(light_pattern, number_lights)
+    light_pattern[0,number_lights]
+  end
+
+  def pad_lights_right(desired_length, current_length)
+    # p "desired length = #{desired_length} minus #{current_length}"
+
+      length_to_pad = desired_length - current_length
+      # p length_to_pad
+      "O" * length_to_pad
   end
 
 
--- clock_lamps.rb
+++ clock_lamps.rb
@@ -3,11 +3,13 @@ class ClockLamps
   attr_accessor :single_minute_row
   attr_accessor :five_minute_row
   attr_accessor :single_hour_row
+  attr_accessor :five_hour_row
 
 
-  def initialize(single_minute, five_minute, single_hour)
+  def initialize(single_minute, five_minute, single_hour, five_hour)
     @single_minute_row = single_minute
     @five_minute_row = five_minute
     @single_hour_row = single_hour
+    @five_hour_row = five_hour
   end
 end
